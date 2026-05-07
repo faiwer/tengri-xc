354c78d9ed5ea810553ccb65d79bc1e9207f9b37
Stepan Zubashev <szubashev@clickup.com>
2026-05-07 20:38:35 +0200

    vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -8,5 +9,9 @@ export default defineConfig({
     modules: {
       localsConvention: 'camelCase',
     },
+  },
+  test: {
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+    environment: 'node',
   },
 });
