e9dfaef024a5be9b596fa1c580a9a25c2f12e80c
Stepan Zubashev <szubashev@clickup.com>
2026-06-01 13:11:04 +0200

    FE: Make the FreeDistance icon blue

--- client/src/components/icons/FreeDistanceIcon.tsx
+++ client/src/components/icons/FreeDistanceIcon.tsx
@@ -3,19 +3,22 @@ import { cloneElement, type ElementNode } from 'react';
 
 import { iconSvgStyle, type IconProps } from './icon';
 
+const POINT_COLOR = '#0033cb';
+const LINE_COLOR = '#7bbadb';
+
 const svg = (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <path
       d="m18.232 82.593 15.384-49.627"
-      style="fill:#393939;fill-opacity:1;stroke:#dba27b;stroke-width:6;stroke-dasharray:none;stroke-opacity:1"
+      style={`fill:#393939;fill-opacity:1;stroke:${LINE_COLOR};stroke-width:6;stroke-dasharray:none;stroke-opacity:1`}
     />
     <path
       d="m34.142 34.033 36.195 38.245"
-      style="fill:none;fill-opacity:1;stroke:#dba27b;stroke-width:6;stroke-dasharray:none;stroke-opacity:1"
+      style={`fill:none;fill-opacity:1;stroke:${LINE_COLOR};stroke-width:6;stroke-dasharray:none;stroke-opacity:1`}
     />
     <path
       d="m71.51 71.94 10.122-54.567"
-      style="fill:#393939;fill-opacity:1;stroke:#dba27b;stroke-width:6;stroke-dasharray:none;stroke-opacity:1"
+      style={`fill:#393939;fill-opacity:1;stroke:${LINE_COLOR};stroke-width:6;stroke-dasharray:none;stroke-opacity:1`}
     />
     <ellipse
       cx="70.66"
@@ -26,7 +29,7 @@ const svg = (
     />
     <path
       d="M70.947 55.12a15.92 15.92 0 0 0-15.92 15.92 15.92 15.92 0 0 0 15.92 15.92 15.92 15.92 0 0 0 15.92-15.92 15.92 15.92 0 0 0-15.92-15.92m0 5.45a10.47 10.47 0 0 1 10.47 10.47 10.47 10.47 0 0 1-10.47 10.47 10.47 10.47 0 0 1-10.47-10.47 10.47 10.47 0 0 1 10.47-10.47"
-      style="fill:#cb7500;fill-opacity:1;stroke-width:18.4456"
+      style={`fill:${POINT_COLOR};fill-opacity:1;stroke-width:18.4456`}
     />
     <ellipse
       cx="18.273"
@@ -37,7 +40,7 @@ const svg = (
     />
     <path
       d="M18.56 65.703a15.92 15.92 0 0 0-15.92 15.92 15.92 15.92 0 0 0 15.92 15.921 15.92 15.92 0 0 0 15.92-15.92 15.92 15.92 0 0 0-15.92-15.92m0 5.45a10.47 10.47 0 0 1 10.47 10.47 10.47 10.47 0 0 1-10.47 10.47 10.47 10.47 0 0 1-10.471-10.47 10.47 10.47 0 0 1 10.47-10.47"
-      style="fill:#cb7500;fill-opacity:1;stroke-width:18.4456"
+      style={`fill:${POINT_COLOR};fill-opacity:1;stroke-width:18.4456`}
     />
     <ellipse
       cx="33.089"
@@ -48,7 +51,7 @@ const svg = (
     />
     <path
       d="M33.376 18.078a15.92 15.92 0 0 0-15.92 15.92 15.92 15.92 0 0 0 15.92 15.921A15.92 15.92 0 0 0 49.296 34a15.92 15.92 0 0 0-15.92-15.92m0 5.45a10.47 10.47 0 0 1 10.47 10.47 10.47 10.47 0 0 1-10.47 10.47 10.47 10.47 0 0 1-10.47-10.47 10.47 10.47 0 0 1 10.47-10.47"
-      style="fill:#cb7500;fill-opacity:1;stroke-width:18.4456"
+      style={`fill:${POINT_COLOR};fill-opacity:1;stroke-width:18.4456`}
     />
     <ellipse
       cx="81.773"
@@ -59,7 +62,7 @@ const svg = (
     />
     <path
       d="M82.06 1.674a15.92 15.92 0 0 0-15.92 15.92 15.92 15.92 0 0 0 15.92 15.921 15.92 15.92 0 0 0 15.92-15.92 15.92 15.92 0 0 0-15.92-15.92m0 5.45a10.47 10.47 0 0 1 10.47 10.47 10.47 10.47 0 0 1-10.47 10.47 10.47 10.47 0 0 1-10.471-10.47 10.47 10.47 0 0 1 10.47-10.47"
-      style="fill:#cb7500;fill-opacity:1;stroke-width:18.4456"
+      style={`fill:${POINT_COLOR};fill-opacity:1;stroke-width:18.4456`}
     />
   </svg>
 ) as ElementNode;
